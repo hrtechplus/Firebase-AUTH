8ef09e413772dfcf62ede597f3b84528a3095854
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-13 01:16:56 +0530

    <Toaster />

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./component/auth/SignIn";
 import SignUp from "./component/auth/SIgnUp";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <div className="App">
       <SignIn />
       <SignUp />
+      <Toaster />
     </div>
   );
 }
