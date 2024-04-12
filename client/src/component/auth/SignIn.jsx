ccc7d7e3ec2ea87f7c41d5b643fbc3289310d0ab
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-13 00:58:07 +0530

    Update SignIn.jsx

--- client/src/component/auth/SignIn.jsx
+++ client/src/component/auth/SignIn.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 
 import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="sign-in-container">
@@ -22,6 +34,7 @@ export default function SignIn() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <input type="submit" onClick={signIn} />
       </from>
     </div>
   );
