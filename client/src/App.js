18375705dc8ba978a8ef1242d823dc4210c89224
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-13 01:14:42 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SignIn from "./component/auth/SignIn";
-
+import SignUp from "./component/auth/SIgnUp";
 function App() {
   return (
     <div className="App">
       <SignIn />
+      <SignUp />
     </div>
   );
 }
