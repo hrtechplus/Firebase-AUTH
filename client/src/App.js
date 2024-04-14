b8d986bfab34351ef238e280a88eda59a77abe8a
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-15 00:49:01 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import SignIn from "./component/auth/SignIn";
 import SignUp from "./component/auth/SIgnUp";
 import toast, { Toaster } from "react-hot-toast";
 import UserDetails from "./component/auth/UserDetails";
+import TestAuth from "./component/auth/TestAuth";
 function App() {
   const isUser = true; // check for the is user null or not
   return (
     <div className="App">
-      <SignIn />
-      <SignUp />
-
+      {/* <SignIn />
+      <SignUp /> */}
+      <TestAuth />
       <Toaster />
     </div>
   );
