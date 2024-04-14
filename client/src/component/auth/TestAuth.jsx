15d8494c96b2d00d9585c05c6681b64c39b498eb
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-15 00:38:56 +0530

    Update TestAuth.jsx

--- client/src/component/auth/TestAuth.jsx
+++ client/src/component/auth/TestAuth.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { auth, provider, signInWithPopup } from "./config";
 
 export default function TestAuth() {
   const handleGoogleSignup = () => {
-    // Handle Google signup logic here
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   };
 
   return (
