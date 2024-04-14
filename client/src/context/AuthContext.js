7f34fece58f30ee8506dfbed12ae2948499a1383
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-04-14 11:34:07 +0530

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -6,11 +6,20 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
+import { auth } from "../firebase";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+  return (
+    <AuthContext.Provider value={{ googleSignIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const UserAuth = () => {
